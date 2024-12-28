d1be88c092028e3c488ad89aebbdd4b4482ee18b
Shubham Sharma <shubham.sharma@tagntrac.com>
2024-12-28 17:26:14 +0530

    Initial commit

---
+++ expense-management/src/main/java/com/grok/ExpenseManagementApplication.java
@@ -0,0 +1,12 @@
+package com.grok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagementApplication.class, args);
+	}
+}
